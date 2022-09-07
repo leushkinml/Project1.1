dd804c2d06f7bb46bde3e35bbbe509dd616c65ab
leush <leushkinml@mail.ru>
2022-09-07 10:07:00 +0300

    Устранение замечаний 07.09.22

--- src/Employee.java
+++ src/Employee.java
@@ -2,10 +2,10 @@ public class Employee {
     private final String surname;
     private final String name;
     private final String secondName;
-    String companyBranch;
-    int salary;
-    private final Counted counter;
-    static int idOfEmployee;
+    private String companyBranch;
+    private int salary;
+    private static int counter;
+    private static int idOfEmployee;
 
 
     public Employee(String surname, String name, String secondName, String companyBranch, int salary) {
@@ -14,11 +14,14 @@ public class Employee {
         this.secondName = secondName;
         this.companyBranch = companyBranch;
         this.salary = salary;
-        this.counter = new Counted();
-        idOfEmployee = Counted.getId();
+        idOfEmployee = counterAdd();
     }
 
     static Employee[] employer = new Employee[10];
+
+    private int counterAdd() {
+        return counter++;
+    }
 
     public String getSurname() {
         return surname;
@@ -48,7 +51,9 @@ public class Employee {
         this.salary = salary;
     }
 
-    public int getIdOfEmployee() {
+    public static int getCounter() { return counter; }
+
+    public static int getIdOfEmployee() {
         return idOfEmployee;
     }
 
@@ -60,70 +65,7 @@ public class Employee {
                 "3. Отчество: " + secondName + '\n' +
                 "4. Отдел: " + companyBranch + '\n' +
                 "5. Зарплата " + (int) salary +
-                '.' + '\n';
-    }
+                '.' + '\n' + "6. ID сотрудника: " + idOfEmployee;
 
-    public static void printStaffTable() {
-        System.out.println();
-        System.out.println("Штатное расписание компании");
-        System.out.println();
-        System.out.println("Всего сотрудников: " + Counted.getId());
-        System.out.println();
-        for (int i = 0; i < Employee.employer.length; i++)
-            if (Employee.employer[i] == null) {
-                System.out.println("ID сотрудника: " + (i + 1)+ '\n' + "Позиция вакантна.");
-            } else {
-                System.out.println("ID сотрудника: " + (i + 1) + '\n' + Employee.employer[i]);
-            }
-    }
-
-    public static int amountOfSalaryCostsPerMonth() {
-        int summary = 0;
-        for (int i = 0; i < idOfEmployee; i++) {
-            summary += Employee.employer[i].getSalary();
-            }
-        return summary;
-    }
-
-    public static String  staffWithMaxSalary() {
-        String staffWithMax = null;
-        int maxSalary = -1;
-        for (int i = 0; i < idOfEmployee; i++) {
-            if (Employee.employer[i].getSalary() > maxSalary) {
-                maxSalary = Employee.employer[i].getSalary();
-                staffWithMax = Employee.employer[i].getSurname() + " " + Employee.employer[i].getName() + " " + Employee.employer[i].getSecondName() + ", имеющий зарплату " + Employee.employer[i].getSalary() + " руб.";
-            }
-        }
-        return staffWithMax;
-    }
-
-    public static String  staffWithMinSalary() {
-        String staffWithMin = null;
-        int minSalary = 1000000000;
-        for (int i = 0; i < idOfEmployee; i++) {
-            if (Employee.employer[i].getSalary() < minSalary) {
-                minSalary = Employee.employer[i].getSalary();
-                staffWithMin = Employee.employer[i].getSurname() + " " + Employee.employer[i].getName() + " " + Employee.employer[i].getSecondName() + ", имеющий зарплату " + Employee.employer[i].getSalary() + " руб.";
-            }
-        }
-        return staffWithMin;
-    }
-
-    public static int averageSalaryPerMonth() {
-        int average = amountOfSalaryCostsPerMonth() / idOfEmployee;
-
-        return average;
-    }
-
-    public static void printFullNameOfStaff() {
-        System.out.println();
-        System.out.println("Фамилия, Имя, Отчество действующих сотрудников компании:");
-        System.out.println();
-        for (int j = 0; j < Employee.employer.length; j++)
-            if (Employee.employer[j] == null) {
-                System.out.println("Позиция вакантна.");
-            } else {
-                System.out.println(Employee.employer[j].getSurname() + " " + Employee.employer[j].getName() + " " + Employee.employer[j].getSecondName() + ".");
-            }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        Employee[] employer = new Employee[10];
+
+
         Employee ivanovIvan = new Employee("Иванов", "Иван", "Иванович", "1", 51_500);
         Employee.employer[0] = ivanovIvan;
         Employee petrovPetr = new Employee("Петров", "Пётр", "Петрович", "5", 112_300);
@@ -14,22 +17,92 @@ public class Main {
         Employee vladimirMayakovskiy = new Employee("Маяковский", "Владимир", "Владимирович", "2", 300_500);
         Employee.employer[5] = vladimirMayakovskiy;
 
+        System.out.println(Employee.employer[4]);
 
-        Employee.printStaffTable(); // Получить cписок всех сотрудников со всеми имеющимися по ним данными.
+        printStaffTable(); // Получить cписок всех сотрудников со всеми имеющимися по ним данными.
+
+        System.out.println("Сумма затрат на зарплаты в месяц: " + amountOfSalaryCostsPerMonth()+ " руб."); // Посчитать сумму затрат на зарплаты в месяц.
         System.out.println();
 
-        System.out.println("Сумма затрат на зарплаты в месяц: " + Employee.amountOfSalaryCostsPerMonth()+ " руб."); // Посчитать сумму затрат на зарплаты в месяц.
+        System.out.println("Сотрудник с минимальной зарплатой в месяц: " + staffWithMinSalary()); // Найти сотрудника с минимальной зарплатой.
         System.out.println();
 
-        System.out.println("Сотрудник с минимальной зарплатой в месяц: " + Employee.staffWithMinSalary()); // Найти сотрудника с минимальной зарплатой.
+        System.out.println("Сотрудник с максимальной зарплатой в месяц: " + staffWithMaxSalary()); // Найти сотрудника с максимальной зарплатой.
         System.out.println();
 
-        System.out.println("Сотрудник с максимальной зарплатой в месяц: " + Employee.staffWithMaxSalary()); // Найти сотрудника с максимальной зарплатой.
+        System.out.println("Среднее значение зарплат в месяц: " + averageSalaryPerMonth() + " руб."); // Подсчитать среднее значение зарплат.
         System.out.println();
 
-        System.out.println("Среднее значение зарплат в месяц: " + Employee.averageSalaryPerMonth() + " руб."); // Подсчитать среднее значение зарплат.
-        System.out.println();
+        printFullNameOfStaff(); // Получить Ф. И. О. всех сотрудников
+    }
 
-        Employee.printFullNameOfStaff(); // Получить Ф. И. О. всех сотрудников
+    private static void printStaffTable() {
+        System.out.println();
+        System.out.println("Штатное расписание компании");
+        System.out.println();
+        System.out.println("Всего сотрудников: " + Employee.getCounter());
+        System.out.println();
+        for (int i = 0; i < Employee.employer.length; i++)
+            if (Employee.employer[i] == null) {
+                System.out.println("ID сотрудника: " + (i + 1)+ '\n' + "Позиция вакантна.");
+            } else {
+                System.out.println("ID сотрудника: " + (i + 1) + '\n' + Employee.employer[i]);
+            }
+        System.out.println();
+    }
+    private static int amountOfSalaryCostsPerMonth() {
+        int summary = 0;
+        for (int i = 0; i < Employee.employer.length; i++) {
+            if (Employee.employer[i] != null) {
+                summary += Employee.employer[i].getSalary();
+            }
+        }
+        return summary;
+    }
+    private static Employee staffWithMinSalary() {
+        //String staffWithMin = null;
+        int minSalary = 1000000000;
+        int i;
+        for (i = 0; i < Employee.employer.length-1; i++) {
+            if (Employee.employer[i] != null) {
+                if (Employee.employer[i].getSalary() < minSalary) {
+                    minSalary = Employee.employer[i].getSalary();
+                    //staffWithMin = Employee.employer[i].getSurname() + " " + Employee.employer[i].getName() + " " + Employee.employer[i].getSecondName() + ", имеющий зарплату " + Employee.employer[i].getSalary() + " руб.";
+                }
+            }
+        }
+        return Employee.employer[i];
+    }
+
+    public static String  staffWithMaxSalary() {
+        String staffWithMax = null;
+        int maxSalary = -1;
+        for (int i = 0; i < Employee.employer.length; i++) {
+            if (Employee.employer[i] != null) {
+                if (Employee.employer[i].getSalary() > maxSalary) {
+                maxSalary = Employee.employer[i].getSalary();
+                staffWithMax = Employee.employer[i].getSurname() + " " + Employee.employer[i].getName() + " " + Employee.employer[i].getSecondName() + ", имеющий зарплату " + Employee.employer[i].getSalary() + " руб.";
+                }
+            }
+        }
+        return staffWithMax;
+    }
+
+   private static double averageSalaryPerMonth() {
+        double average = amountOfSalaryCostsPerMonth() / Employee.employer.length;
+
+        return average;
+   }
+
+    private static void printFullNameOfStaff() {
+        System.out.println();
+        System.out.println("Фамилия, Имя, Отчество действующих сотрудников компании:");
+        System.out.println();
+        for (int j = 0; j < Employee.employer.length; j++)
+            if (Employee.employer[j] == null) {
+                System.out.println("Позиция вакантна.");
+            } else {
+                System.out.println(Employee.employer[j].getSurname() + " " + Employee.employer[j].getName() + " " + Employee.employer[j].getSecondName() + ".");
+            }
     }
 }
